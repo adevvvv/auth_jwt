c170dad6090eb5d36214dd9d168b4bc68d31a2ae
adevvvv <adevvv@bk.ru>
2024-03-31 20:19:53 +0300

    feature: Добавь класс подключение к БД

--- config/Database.php
+++ config/Database.php
@@ -1,1 +1,24 @@
 <?php
+
+class Database
+{
+    private $host = "localhost";
+    private $dbName = "authentication_jwt";
+    private $userName = "root";
+    private $password = "";
+    public $connect;
+
+    // Получаем соединение с базой данных
+    public function getConnection(): ?PDO
+    {
+        $this->connect = null;
+
+        try {
+            $this->connect = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbName, $this->userName, $this->password);
+        } catch (PDOException $exception) {
+            echo "Ошибка соединения с БД: " . $exception->getMessage();
+        }
+
+        return $this->connect;
+    }
+}
